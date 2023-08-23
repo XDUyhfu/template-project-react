553e733fe356f0be71fc2678ab9e2bedef19afa8
yhfu <yhfu@stu.xidian.edu.cn>
2023-08-24 00:13:12 +0800

    feat: 添加用户信息页面

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@ import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import eslint from 'vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react(), eslint()],
+    plugins: [
+        react(),
+        eslint(),
+        stylelint({
+            // 对某些文件排除检查
+            exclude: ['node_modules'],
+            cache: false
+        })
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
